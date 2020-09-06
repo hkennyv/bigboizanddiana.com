e973820349595bc4dc5e2d28aeed3505e4c7dade
danielmiranda <manieldiranda@gmail.com>
2020-09-05 17:10:16 -0700

    implemented kenny's feedback

--- web/pages/index.js
+++ web/pages/index.js
@@ -3,6 +3,7 @@ import {Container} from 'semantic-ui-react';
 import DiscordStats from '../components/DiscordStats';
 import React, {Component} from 'react';
 import {Table} from 'semantic-ui-react'
+import DiscordTable from '../components/DiscordTable';
 
 
 const {API_URL} = process.env;
@@ -16,7 +17,8 @@ class Home extends Component {
                 channels: {}
 
             },
-            isLoading: true
+            isLoading: true,
+            activeTable:"general"
         }
     }
 
@@ -25,7 +27,7 @@ class Home extends Component {
     }
 
     getDiscordServerData = () => {
-        fetch("http://localhost:8080")
+        fetch('http://localhost:8080')
             .then(res => res.json())
             .then(
                 (result) => {
@@ -33,7 +35,14 @@ class Home extends Component {
                         data: result.data,
                         isLoading: false
                     });
-                })
+                }
+            )
+    }
+
+    tableButtonPress = (tableName) => {
+        this.setState({
+            activeTable: tableName
+        })
     }
 
     render() {
@@ -48,39 +57,24 @@ class Home extends Component {
                     <main>
                         <div className={'headingTitleContainer'}>
                             <h1>{this.state.data.guild}</h1>
-                            {/*<img  className="titleImage" src={this.state.data.guildIconURL}/>*/}
                         </div>
                         <p className={'titleSubHeading'}>
                             Join us in the <a>discord!</a>
                         </p>
                         <div>
-                            {Object.keys(this.state.data.channels).map(channel => (
-                                <div className={'dataTableContainer'}>
-                                    <h2 style={{textAlign: 'center'}}> #{channel} </h2>
-                                    <Table celled className={'dataTable'}>
-                                        <Table.Header>
-                                            <Table.Row>
-                                                <Table.HeaderCell>Name</Table.HeaderCell>
-                                                <Table.HeaderCell>Contributions</Table.HeaderCell>
-                                            </Table.Row>
-                                        </Table.Header>
-                                        <Table.Body>
-                                            {Object.keys(this.state.data.channels[channel].stats).map(key => (
-                                                <Table.Row>
-                                                    <Table.Cell className={'userTableCell'}>
-                                                        <img className={'tableImage'} src={this.state.data.channels[channel].users[key]}/>
-                                                        <p> {key} </p>
-                                                    </Table.Cell>
-                                                    <Table.Cell><p>
-                                                        {this.state.data.channels[channel].stats[key]}</p>
-                                                    </Table.Cell>
-                                                </Table.Row>
-                                            ))}
-                                        </Table.Body>
-                                    </Table>
-                                </div>
-                            ))}
-                            <DiscordStats data={this.state.data} isLoading={this.state.isLoading}/>
+                            {/* Loops through data, and renders a button for every channel in this.state.data*/}
+                            <div className={'buttonContainer'}>
+                                {Object.keys(this.state.data.channels).map(key => (
+                                    // when you click a button, it changes active button in state, telling the discordtable component to render
+                                    // data for that channel
+                                    <button
+                                        onClick={(e) => this.tableButtonPress(key)}
+                                        className={'ui button' + (this.state.activeTable === key ? ' activeChannelButton' : '')}
+                                    > {key} </button>
+                                ))}
+                            </div>
+                            <DiscordTable activeTable={this.state.activeTable} channelData={this.state.data.channels[this.state.activeTable]} isLoading={this.state.isLoading}/>
+                            {/*<DiscordStats data={this.state.data} isLoading={this.state.isLoading}/>*/}
                         </div>
                     </main>
                 </Container>
